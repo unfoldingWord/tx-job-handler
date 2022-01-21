60da6ecedb6c0dca5de96d2fb96fe2d804db133b
Richard Mahn <richmahn@users.noreply.github.com>
2022-01-21 12:03:47 -0500

    Changes for dcs_api.py

--- door43_tools/dcs_api.py
+++ door43_tools/dcs_api.py
@@ -1,8 +1,11 @@
 import requests
-import giteapy
+import dcs_api_client
+import dcs_catalog_client
 import yaml
-from giteapy.rest import ApiException
+import base64
+from dcs_api_client.rest import ApiException
 from urllib.parse import urlencode
+from pprint import pprint
 
 DEFAULT_BRANCH = 'master'
 PRODUCTION_DCS_DOMAIN = "https://qa.door43.org"
@@ -16,35 +19,38 @@ class DcsApi(object):
                  catalog_version=CATALOG_NEXT_VERSION, debug=False):
         self.dcs_domain = dcs_domain
         if dcs_domain:
-            self.cs_domain = dcs_domain
+            self.dcs_domain = dcs_domain
         elif debug:
             self.dcs_domain = QA_DCS_DOMAIN
         else:
             self.dcs_domain = PRODUCTION_DCS_DOMAIN
 
-        self.api_base_url = f"{dcs_domain}/api/v{api_version}"
-        self.catalog_url = f"{dcs_domain}/api/catalog/v{catalog_version}"
-        self.api_config = giteapy.Configuration()
+        self.api_base_url = f"{self.dcs_domain}/api/v{api_version}"
+        self.catalog_url = f"{self.dcs_domain}/api/catalog/v{catalog_version}"
+        self.api_config = dcs_api_client.Configuration()
         self.api_config.host = self.api_base_url
-        self.gitea_api = giteapy.ApiClient(configuration=self.api_config)
-        self.repo_api = giteapy.RepositoryApi(api_client=self.gitea_api)
+        self.api_client = dcs_api_client.ApiClient(configuration=self.api_config)
+        self.repo_api = dcs_api_client.RepositoryApi(api_client=self.api_client)
+        self.catalog_config = dcs_catalog_client.Configuration()
+        self.catalog_config.host = self.api_base_url
+        self.catalog_client = dcs_catalog_client.ApiClient(configuration=self.catalog_config)
+        self.catalog_api = dcs_catalog_client.V5Api(api_client=self.api_client)
 
-    def get_manifest(self, owner, repo_name):
+    def get_manifest(self, owner, repo_name, ref):
         try:
-            # Currently just gets the manifest of the master branch
-            # Todo: when DCS goes to Gitea 1.14 and giteapy adds ref= to the raw file contents param, add to the below
-            response = self.repo_api.repo_get_raw_file(owner, repo_name, 'manifest.yaml')
-            return yaml.safe_load(response.data)
+            response = self.repo_api.repo_get_contents(owner, repo_name, "manifest.yaml", ref=ref)
+            return yaml.safe_load(base64.b64decode(response.content))
         except ApiException as e:
             print("Exception when calling RepositoryApi->repo_get_raw_file: %s\n" % e)
             return None
 
     def get_repo(self, owner, repo_name):
-        url = f"{self.api_base_url}/repos/{owner}/{repo_name}"
-        print(url)
-        response = requests.get(url)
-        return response.json()
-
+        try:
+            # Get a repository
+           return self.repo_api.repo_get(owner, repo_name)
+        except ApiException as e:
+            print("Exception when calling RepositoryApi->repo_get: %s\n" % e)
+            return None
 
     def get_catalog_entry(self, owner, repo_name, ref=None):
         if not ref:
