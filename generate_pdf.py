24cf1a4ea09a281842accdf77bf041453be5422c
Richard Mahn <richmahn@users.noreply.github.com>
2023-05-01 13:30:59 -0600

    Updates generate_pdf.py

--- generate_pdf.py
+++ generate_pdf.py
@@ -111,6 +111,7 @@ def generate_pdf(repo_name, owner='unfoldingWord', ref=None, dcs_domain='git.doo
             "repo_name": repo_name,
             "repo_owner": owner,
             "repo_ref": ref,
+            "repo_ref_type": "branch",
             "repo_data_url": f"https://{dcs_domain}/{owner}/{repo_name}/archive/{ref}.zip",
             "dcs_domain": f"https://{dcs_domain}",
             "project_ids": project_ids,
